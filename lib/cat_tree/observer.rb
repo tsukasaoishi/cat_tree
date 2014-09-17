bd90275c85c5f6cffcf3145bf0fc651f803d8be2
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2014-09-17 11:46:33 +0900

    add AR::Relation count spec

--- lib/cat_tree/observer.rb
+++ lib/cat_tree/observer.rb
@@ -14,30 +14,24 @@ module CatTree
     def notice(object, options = {})
       case object
       when ActiveRecord::Base
-        key = "#{object.class.name}_#{object.id}"
-        @ar_base[key] += 1
+        notice_from_base(object, options)
       when ActiveRecord::Relation
-        return if caller.detect{|c| c =~ /`build_default_scope'$/ }
-
-        ids = [object.object_id, options[:source_id]].compact
-        if key = @ar_relation.keys.detect{|k| (k - ids).size != k.size }
-          val = @ar_relation.delete(key)
-          key.concat(ids).uniq!
-        else
-          val = {}
-          key = ids
-        end
-        
-        @ar_relation[key] = val
-        @ar_relation[key][:object] = object
-        @ar_relation[key][:caller] = caller
+        notice_from_relation(object, options)
       else
         raise ArgumentError, "unknown object : #{object.inspect}"
       end
     end
 
-    def count
+    def ar_base_count
       @ar_base.values.inject(0){|t,c| t + c}
+    end
+
+    def ar_relation_count
+      @ar_relation.size
+    end
+
+    def unused_ar_relation_count
+      @ar_relation.select{|k,v| !v[:object].loaded?}.size
     end
 
     def check
@@ -45,6 +39,30 @@ module CatTree
       yield
     ensure
       ActiveRecord::Base.remove_cat_tree_observer
+    end
+
+    private
+
+    def notice_from_base(object, options)
+      key = "#{object.class.name}_#{object.id}"
+      @ar_base[key] += 1
+    end
+
+    def notice_from_relation(object, options)
+      return if caller.detect{|c| c =~ /`build_default_scope'$/ }
+
+      ids = [object.object_id, options[:source_id]].compact
+      if key = @ar_relation.keys.detect{|k| (k - ids).size != k.size }
+        val = @ar_relation.delete(key)
+        key.concat(ids).uniq!
+      else
+        val = {}
+        key = ids
+      end
+
+      @ar_relation[key] = val
+      @ar_relation[key][:object] = object
+      @ar_relation[key][:caller] = caller
     end
   end
 end
--- spec/unit/observer_spec.rb
+++ spec/unit/observer_spec.rb
@@ -8,26 +8,52 @@ describe CatTree::Observer do
 
   context "#check" do
     it "check count to create AR::Base object" do
-      number_of_object = 1#rand(10) + 1
+      number_of_object = rand(10) + 1
       observer = CatTree::Observer.new
       observer.check do
         number_of_object.times { User.new }
       end
 
-      expect(observer.count).to eq(number_of_object)
+      expect(observer.ar_base_count).to eq(number_of_object)
     end
 
     it "check count to find AR::Base object" do
-      number_of_object = 1#rand(10) + 1
+      number_of_object = rand(10) + 1
       User.create(:name => "cat_tree")
 
       observer = CatTree::Observer.new
       observer.check do
-        a = User.where(:name => "cat_tree")
         number_of_object.times { User.where(:name => "cat_tree").first }
       end
 
-      expect(observer.count).to eq(number_of_object)
+      expect(observer.ar_base_count).to eq(number_of_object)
+    end
+
+    it "check count to create AR::Relation object" do
+      number_of_object = rand(10) + 1
+      User.create(:name => "cat_tree")
+
+      observer = CatTree::Observer.new
+      observer.check do
+        number_of_object.times { User.where(:name => "cat_tree").first }
+      end
+
+      expect(observer.ar_relation_count).to eq(number_of_object)
+    end
+
+    it "check count un-used AR::Relation object" do
+      number_of_object = rand(10) + 1
+      User.create(:name => "cat_tree")
+
+      observer = CatTree::Observer.new
+      observer.check do
+        number_of_object.times do
+          User.where(:name => "cat_tree").first
+          User.where(:name => "cat_tree").limit(1)
+        end
+      end
+
+      expect(observer.unused_ar_relation_count).to eq(number_of_object)
     end
   end
 end
